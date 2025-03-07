2d8bd0338058c99fb2ccf131bdc8ffc3a72132da
tkjelsrud <tkjelsrud@gmail.com>
2025-03-07 19:55:09 +0100

    .

--- web/app.py
+++ web/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     w = get_weather(CONFIG['LAT'], CONFIG['LON'])
 
-    return "<p>Hello, World!</p><br/>" + w
+    return "<p>Hello, World!</p><br/>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
