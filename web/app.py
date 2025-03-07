fdfdc08b94b84917034dd49a33f7f9d84643c6a6
tkjelsrud <tkjelsrud@gmail.com>
2025-03-07 19:46:35 +0100

    .

--- web/app.py
+++ web/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from web.routes import main_blueprint
 
 app = Flask(__name__)
-app.config.from_object("web.config")
 
-# Register routes
-app.register_blueprint(main_blueprint)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
