4c5c83d792de97c268ddd22123fcd456ba94c5a5
tkjelsrud <tkjelsrud@gmail.com>
2025-12-01 15:41:20 +0100

    Fix imports in web/app.py for PI server

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory, render_template, jsonify
-from web.routes import routes
-from web.config import CONFIG
+from routes import routes
+from config import CONFIG
 
 
 app = Flask(__name__)
